a0d38acbe7213cbe09841a242c4b9a17dd1ea882
mishrankit30 <mishraas2001@gmail.com>
2025-12-28 14:59:55 +0000

    A descriptive message for your changes in backend.groovy

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -1,9 +1,11 @@
-pipeline{
+pipeline {
     agent any
-    stages{
-        stage{'Code-pull'}{
-            steps{
-                git branch: 'main', url: 'https://github.com/mishrankit30/flight-reservation-app.git'
+
+    stages {
+        stage('Code-pull') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/mishrankit30/flight-reservation-app.git'
             }
         }
     }
