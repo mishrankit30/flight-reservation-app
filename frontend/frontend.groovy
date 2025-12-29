3ab6aa9e07f4102bede66b04d0d76076345d54e2
mishrankit30 <mishraas2001@gmail.com>
2025-12-29 17:48:01 +0000

    error solve

--- frontend/frontend.groovy
+++ frontend/frontend.groovy
@@ -1,12 +1,14 @@
 pipeline{
     agent any
+
     stages{
+        
         stage('code-pull'){
            steps{
                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
            }
         }
-        stage{'build'}{
+        stage{'Build'}{
             steps{
                 sh '''
                     cd frontend
