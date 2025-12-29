9d84ddecc3f1c0c36de9f267d3bf3388979a406c
mishrankit30 <mishraas2001@gmail.com>
2025-12-29 16:26:38 +0000

    error solve

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -28,7 +28,7 @@ pipeline {
         stage('Docker-build'){
             steps{
                 sh '''
-                    cd cd FlightReservationApplication
+                    cd FlightReservationApplication
                     docker build . -t mishrankit30/flightreservation-new:latest
                     docker push mishrankit30/flightreservation-new:latest
                     docker rmi mishrankit30/flightreservation-new:latest
