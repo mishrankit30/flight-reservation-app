9d0d83e2d85e6b646a29c51a9516a3df137838f0
mishrankit30 <mishraas2001@gmail.com>
2025-12-29 17:51:47 +0000

    error solve

--- frontend/frontend.groovy
+++ frontend/frontend.groovy
@@ -1,15 +1,16 @@
-pipeline{
+pipeline {
     agent any
 
-    stages{
-        
-        stage('code-pull'){
-           steps{
-               git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
-           }
+    stages {
+
+        stage('code-pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+            }
         }
-        stage{'Build'}{
-            steps{
+
+        stage('Build') {
+            steps {
                 sh '''
                     cd frontend
                     npm install
@@ -17,11 +18,12 @@ pipeline{
                 '''
             }
         }
-        stage('Deplyo'){
-            stepd{
+
+        stage('Deploy') {
+            steps {
                 sh '''
-                   cd frontend
-                   aws s3 sync dist/ s3://cbz-frontend-project/
+                    cd frontend
+                    aws s3 sync dist/ s3://cbz-frontend-project/
                 '''
             }
         }
